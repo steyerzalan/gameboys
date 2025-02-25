6efc4078207c5f4c7558bc52aac7e246d44e75b0
steyerzalan <steyerzalan@gmail.com>
2025-02-25 12:58:51 +0100

    Games.php half piece

---
+++ Games.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'dbvezerlo.php';
+
+class Games
+{
+    private $games=[];
+
+    public function __construct()
+    {
+        $query=" SELECT `g_ID`, name, pic FROM Games";
+        $dbvez= new DBVezerlo();
+        $this->games=$dbvez->executeSelectQuery($query);
+        $dbvez->closeDB();
+    }
+
+    public function getAllGames()
+    {
+        return $this->games;
+    }
+
+    public function getGamesById($GameId)
+    {
+        $query = " SELECT g_ID, name, pic From Games
+         Where g_ID=".$GameId;
+         $dbvez = new DBVezerlo();
+         $this->games=$dbvez->executeSelectQuery($query);
+         $dbvez->closeDB();
+         return $this->games;
+    }
+    
+}
