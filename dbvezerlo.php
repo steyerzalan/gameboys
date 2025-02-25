0a27ab9dc04f5512c743202c3dd191a24b40c89a
steyerzalan <steyerzalan@gmail.com>
2025-02-25 12:03:24 +0100

    dbvezerlo.php elkészült tesztre

---
+++ dbvezerlo.php
@@ -0,0 +1,45 @@
+<?php
+class DBVezerlo {
+    private $conn=null;
+    private $host="localhost";
+
+    private $user="root";
+
+    private $password="";
+
+    private $database="gameboy";
+
+    function __construct()
+    {
+        $conn=$this->connectDB();
+        if (!empty($conn))
+            $this->conn=$conn;
+    }
+
+    function connectDB()
+    {
+        $conn=mysqli_connect($this->host, $this->user, $this->password,
+        $this->database);
+        return $conn;
+    }
+
+    function closeDB()
+    {
+        if(!empty($this->conn))
+        {
+            mysqli_close($this->conn);
+            $this->conn=null;
+        }
+    }
+
+    function executeSelectQuery($query)
+    {
+        $result=mysqli_query($this->conn, $query);
+        while($row=mysqli_fetch_assoc($result))
+        {
+            $resultset[]=$row;
+        }
+        if (!empty($resultset))
+            return $resultset;
+    }
+}
